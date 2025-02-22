95e2fac5cd94440289fc68887279ea5e8d75c21c
Bradley Morin <bradleymorin@gmail.com>
2025-02-22 12:20:51 -0700

    Add caesar cipher

--- practice.js
+++ practice.js
@@ -1,13 +1,59 @@
 export function capitalize(string) {
-    if (typeof string !== 'string' || string.length === 0) {
-        return string;
-    }
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  if (typeof string !== "string" || string.length === 0) {
+    return string;
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function reverseString(str) {
-    if (typeof str !== 'string' || str.length === 0) {
-        return str;
-    }
-    return str.split('').reverse().join('');
+  if (typeof str !== "string" || str.length === 0) {
+    return str;
+  }
+  return str.split("").reverse().join("");
+}
+
+export const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+export function caesarCipher(str, shift) {
+  if (typeof str !== "string") {
+    return str;
+  }
+  return str
+    .split("")
+    .map((char) => shiftChar(char, shift))
+    .join("");
+}
+
+function shiftChar(char, shift) {
+  const code = char.charCodeAt(0);
+  if (isLowerCase(code)) {
+    return shiftCharCode(code, shift, 97, 122);
+  } else if (isUpperCase(code)) {
+    return shiftCharCode(code, shift, 65, 90);
+  } else {
+    return char;
+  }
+}
+
+function isLowerCase(code) {
+  return code >= 97 && code <= 122;
+}
+
+function isUpperCase(code) {
+  return code >= 65 && code <= 90;
+}
+
+function shiftCharCode(code, shift, min, max) {
+  let shiftedCode = code + (shift % 26);
+  if (shiftedCode > max) {
+    shiftedCode -= 26;
+  } else if (shiftedCode < min) {
+    shiftedCode += 26;
+  }
+  return String.fromCharCode(shiftedCode);
 }
--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,4 @@
-import {capitalize, reverseString} from './practice';
+import {capitalize, reverseString, calculator, caesarCipher} from './practice';
 
 test('capitalizes hello world', () => {
     expect(capitalize("hello, world!")).toBe("Hello, world!");
@@ -6,4 +6,23 @@ test('capitalizes hello world', () => {
 
 test('reverses hello world', () => {
     expect(reverseString("Hello, world!")).toBe("!dlrow ,olleH");
+})
+
+describe('calculator', () => {
+    test('add: adds two numbers', () => {
+        expect(calculator.add(2, 3)).toBe(5);
+    });
+    test('subtract: subtracts two numbers', () => {
+        expect(calculator.subtract(5, 3)).toBe(2);
+    });
+    test('multiply: multiplies two numbers', () => {
+        expect(calculator.multiply(2, 4)).toBe(8);
+    });
+    test('divide: divides two numbers', () => {
+        expect(calculator.divide(10, 2)).toBe(5);
+    });
+})
+
+test('ciphers hello world', () => {
+    expect(caesarCipher("Hello, World!", 4)).toBe("Lipps, Asvph!");
 })
